37408e0666f8610835b3f94ae09e4eb313451c97
wujunchao <wujunchao@aidigger.com>
2021-03-16 16:39:02 +0800

    Add StringToInt8/16/32/64

--- part_string.go
+++ part_string.go
@@ -42,6 +42,30 @@ func StringToInt(s string) int {
 	return i
 }
 
+// StringToInt8 converts a string to an int8, errors are ignored
+func StringToInt8(s string) int8 {
+	i, _ := strconv.ParseInt(s, 10, 8)
+	return int8(i)
+}
+
+// StringToInt16 converts a string to an int16, errors are ignored
+func StringToInt16(s string) int16 {
+	i, _ := strconv.ParseInt(s, 10, 16)
+	return int16(i)
+}
+
+// StringToInt32 converts a string to an int32, errors are ignored
+func StringToInt32(s string) int32 {
+	i, _ := strconv.ParseInt(s, 10, 32)
+	return int32(i)
+}
+
+// StringToInt64 converts a string to an int64, errors are ignored
+func StringToInt64(s string) int64 {
+	i, _ := strconv.ParseInt(s, 10, 64)
+	return i
+}
+
 // StringToUint converts a string to an uint, errors are ignored
 func StringToUint(s string) uint {
 	i, _ := strconv.Atoi(s)
@@ -49,6 +73,30 @@ func StringToUint(s string) uint {
 		return 0
 	}
 	return uint(i)
+}
+
+// StringToUint8 converts a string to an uint8, errors are ignored
+func StringToUint8(s string) uint8 {
+	i, _ := strconv.ParseUint(s, 10, 8)
+	return uint8(i)
+}
+
+// StringToUint16 converts a string to an uint16, errors are ignored
+func StringToUint16(s string) uint16 {
+	i, _ := strconv.ParseUint(s, 10, 16)
+	return uint16(i)
+}
+
+// StringToUint32 converts a string to an uint32, errors are ignored
+func StringToUint32(s string) uint32 {
+	i, _ := strconv.ParseUint(s, 10, 32)
+	return uint32(i)
+}
+
+// StringToUint64 converts a string to an uint64, errors are ignored
+func StringToUint64(s string) uint64 {
+	i, _ := strconv.ParseUint(s, 10, 64)
+	return i
 }
 
 const (
--- part_string_test.go
+++ part_string_test.go
@@ -1,8 +1,9 @@
 package magicconch
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
-
+	"math"
 	"testing"
 )
 
@@ -38,6 +39,48 @@ func TestStringToInt(t *testing.T) {
 	assertions.Equal(0, StringToInt("a"))
 }
 
+func TestStringToInt8(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(int8(1), StringToInt8("1"))
+	assertions.Equal(int8(0), StringToInt8("0"))
+	assertions.Equal(int8(-1), StringToInt8("-1"))
+	assertions.Equal(int8(0), StringToInt8("-0"))
+	assertions.Equal(int8(0), StringToInt8("a"))
+	assertions.Equal(int8(math.MaxInt8), StringToInt8(fmt.Sprintf("%d", math.MaxInt8+1)))
+}
+
+func TestStringToInt16(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(int16(1), StringToInt16("1"))
+	assertions.Equal(int16(0), StringToInt16("0"))
+	assertions.Equal(int16(-1), StringToInt16("-1"))
+	assertions.Equal(int16(0), StringToInt16("-0"))
+	assertions.Equal(int16(0), StringToInt16("a"))
+	assertions.Equal(int16(math.MaxInt16), StringToInt16(fmt.Sprintf("%d", math.MaxInt16+1)))
+}
+
+func TestStringToInt32(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(int32(1), StringToInt32("1"))
+	assertions.Equal(int32(0), StringToInt32("0"))
+	assertions.Equal(int32(-1), StringToInt32("-1"))
+	assertions.Equal(int32(0), StringToInt32("-0"))
+	assertions.Equal(int32(0), StringToInt32("a"))
+	assertions.Equal(int32(math.MaxInt32), StringToInt32(fmt.Sprintf("%d", math.MaxInt32+1)))
+}
+
+func TestStringToInt64(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(int64(1), StringToInt64("1"))
+	assertions.Equal(int64(0), StringToInt64("0"))
+	assertions.Equal(int64(-1), StringToInt64("-1"))
+	assertions.Equal(int64(0), StringToInt64("-0"))
+	assertions.Equal(int64(0), StringToInt64("a"))
+	// math.MaxInt64 == 9223372036854775807
+	assertions.Equal(int64(math.MaxInt64), StringToInt64("9223372036854775807"))
+	assertions.Equal(int64(math.MaxInt64), StringToInt64("9223372036854775808"))
+}
+
 func TestStringToUint(t *testing.T) {
 	assertions := require.New(t)
 	assertions.Equal(uint(1), StringToUint("1"))
@@ -45,6 +88,48 @@ func TestStringToUint(t *testing.T) {
 	assertions.Equal(uint(0), StringToUint("-1"))
 	assertions.Equal(uint(0), StringToUint("-0"))
 	assertions.Equal(uint(0), StringToUint("a"))
+}
+
+func TestStringToUint8(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(uint8(1), StringToUint8("1"))
+	assertions.Equal(uint8(0), StringToUint8("0"))
+	assertions.Equal(uint8(0), StringToUint8("-1"))
+	assertions.Equal(uint8(0), StringToUint8("-0"))
+	assertions.Equal(uint8(0), StringToUint8("a"))
+	assertions.Equal(uint8(math.MaxUint8), StringToUint8(fmt.Sprintf("%d", math.MaxUint8+1)))
+}
+
+func TestStringToUint16(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(uint16(1), StringToUint16("1"))
+	assertions.Equal(uint16(0), StringToUint16("0"))
+	assertions.Equal(uint16(0), StringToUint16("-1"))
+	assertions.Equal(uint16(0), StringToUint16("-0"))
+	assertions.Equal(uint16(0), StringToUint16("a"))
+	assertions.Equal(uint16(math.MaxUint16), StringToUint16(fmt.Sprintf("%d", math.MaxUint16+1)))
+}
+
+func TestStringToUint32(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(uint32(1), StringToUint32("1"))
+	assertions.Equal(uint32(0), StringToUint32("0"))
+	assertions.Equal(uint32(0), StringToUint32("-1"))
+	assertions.Equal(uint32(0), StringToUint32("-0"))
+	assertions.Equal(uint32(0), StringToUint32("a"))
+	assertions.Equal(uint32(math.MaxUint32), StringToUint32(fmt.Sprintf("%d", math.MaxUint32+1)))
+}
+
+func TestStringToUint64(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal(uint64(1), StringToUint64("1"))
+	assertions.Equal(uint64(0), StringToUint64("0"))
+	assertions.Equal(uint64(0), StringToUint64("-1"))
+	assertions.Equal(uint64(0), StringToUint64("-0"))
+	assertions.Equal(uint64(0), StringToUint64("a"))
+	// math.MaxUint64 == 18446744073709551615
+	assertions.Equal(uint64(math.MaxUint64), StringToUint64("18446744073709551615"))
+	assertions.Equal(uint64(math.MaxUint64), StringToUint64("18446744073709551616"))
 }
 
 func TestStringRand(t *testing.T) {
