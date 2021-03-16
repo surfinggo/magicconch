509c73923b3ddb5ed4e094f4309b4b0cda2cd604
wujunchao <wujunchao@aidigger.com>
2021-03-16 16:46:08 +0800

    Add IntInSlice and UintInSlice

--- part_number.go
+++ part_number.go
@@ -12,3 +12,23 @@ var Uint8Ptr = PtrToUint8
 var Uint16Ptr = PtrToUint16
 var Uint32Ptr = PtrToUint32
 var Uint64Ptr = PtrToUint64
+
+// IntInSlice determines whether an int is in a slice or not.
+func IntInSlice(i int, slice []int) bool {
+	for _, s := range slice {
+		if s == i {
+			return true
+		}
+	}
+	return false
+}
+
+// UintInSlice determines whether an uint is in a slice or not.
+func UintInSlice(i uint, slice []uint) bool {
+	for _, s := range slice {
+		if s == i {
+			return true
+		}
+	}
+	return false
+}
--- part_number_test.go
+++ part_number_test.go
@@ -1,6 +1,9 @@
 package magicconch
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestIntPtr(t *testing.T) {
 	Float32Ptr(1)
@@ -15,4 +18,18 @@ func TestIntPtr(t *testing.T) {
 	Uint16Ptr(1)
 	Uint32Ptr(1)
 	Uint64Ptr(1)
+}
+
+func TestIntInSlice(t *testing.T) {
+	assertions := require.New(t)
+	assertions.False(IntInSlice(1, []int{}))
+	assertions.False(IntInSlice(1, []int{2}))
+	assertions.True(IntInSlice(1, []int{1, 2}))
+}
+
+func TestUintInSlice(t *testing.T) {
+	assertions := require.New(t)
+	assertions.False(UintInSlice(uint(1), []uint{}))
+	assertions.False(UintInSlice(uint(1), []uint{uint(2)}))
+	assertions.True(UintInSlice(uint(1), []uint{uint(1), uint(2)}))
 }
